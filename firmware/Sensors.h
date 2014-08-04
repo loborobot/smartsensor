a1ad0cdb2129409ab003e9a9b761163a90610ffe
Julio Cesar <julhocesar.7@gmail.com>
2014-08-04 17:59:20 +0200

    sensors class redefinition and variables increase

--- firmware/Sensors.h
+++ firmware/Sensors.h
@@ -9,38 +9,24 @@ class Sensors
 public:
   void begin();
   void execute();
+  void updateSensor();
   
-  uint8_t readDataDHT11();
-  byte humidity11;
-  byte temperature11;
-  
-  float readDataLDR();
-  inline float getValueLDR(){return _lastValueLDR;};
-  
-  float readDataUV();
-  inline float getValueUV(){return _lastValueUV;};
-  
-  float readDataSound();
-  
+  uint8_t readDataDHT();
+  uint8_t readDataLDR();
+  uint8_t readDataUV();
+  uint8_t readDataSound();
+  uint8_t readDataFlowmeter();
   volatile int NbTopsFan; //measuring the rising edges of the signal
-  void readDataFlowmeter();
 
 private:
-  uint8_t _pinLDR;
-  float _valueLDR;
-  float _lastValueLDR;
-  //float _minLDR;
-  //float _maxLDR;
-  
-  uint8_t _pinUV;
+  float _humidity;
+  float _temperature;
+  int _valueLDR;
+  int _valLDRmin = 1023;
+  int _valLDRmax = 0;
   float _valueUV;
-  float _lastValueUV;
-  //float _minUV;
-  //float _maxUV;
-  
   float _sound;
- 
-  float flowmeter; 
-  float volume ;
+  float _flowmeter; 
+  float _volume ;
 };
 #endif
