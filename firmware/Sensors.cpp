c605490b4ac904220b8cd5e323db88e3ce104eee
Julio Cesar <julhocesar.7@gmail.com>
2014-08-04 17:57:54 +0200

    redeficion of LDR and UV methods, setting code to 11Kbytes

--- firmware/Sensors.cpp
+++ firmware/Sensors.cpp
@@ -14,54 +14,30 @@ void Sensors::begin(){ //init variables
   attachInterrupt(0,rpm,RISING); //and the interrupt is attached - flowmeter
   
   
+  // calibrate during the first five seconds 
+  while (millis() < 5000) {
+    _valueLDR = analogRead(LDR_PIN);
+    // record the maximum sensor value
+    if (_valueLDR > _valLDRmax) {_valLDRmax = _valueLDR;}
+    // record the minimum sensor value
+    if (_valueLDR < _valLDRmin) {_valLDRmin = _valueLDR;}
+  }
+  
+  
 }
 void Sensors::execute(){ // init program
-  
-  uint8_t chk = readDataDHT11();//Temperature and humidity
-  switch (chk)
-  {
-    case 0: 
-            Serial.print("RH: ");
-            Serial.print(((float)humidity11)/10, 2);Serial.print(" %");
-            Serial.print(" Temperature: ");
-            Serial.print(((float)temperature11)/10, 2);Serial.println(" (oC)");
-            delay(2000);break;
-    case -1: Serial.println("Checksum error"); break;
-    case -2: Serial.println("Time out error"); break;
-    default: Serial.println("Unknown error"); break;
-  }
-  
-  float light = readDataLDR(); //LDR
-  if(light == -2)
-  {
-    Serial.print("LDR (IN): ");
-    Serial.print(getValueLDR());
-    Serial.print (" LDR (OUT): Maxima ilumicacion");
-  }
-  else
-  {  
-    Serial.print("LDR (IN): ");
-    Serial.print(getValueLDR());
-    Serial.print (" LDR (OUT): ");
-    Serial.print(light);  
-    Serial.println(" lux");
-  }
-  
-  float ultra_violet = readDataUV(); // UV
-  Serial.print ("UV (IN): ");
-  Serial.print(getValueUV());
-  Serial.print (" UV (OUT): ");
-  Serial.print(ultra_violet); 
-  Serial.println(" UVI");
-  
-  Serial.print("Sound: "); //Sound
-  Serial.println(readDataSound());
-  
-  readDataFlowmeter();
-  
+  updateSensor();  
 }
 
-uint8_t Sensors::readDataDHT11() // temperature and humidity function
+void Sensors::updateSensor(){
+  uint8_t chk = readDataDHT();// temperature and humidity
+  uint8_t light = readDataLDR(); // LDR
+  uint8_t ultra_violet = readDataUV(); // UV
+  uint8_t sound = readDataSound(); // sound
+  uint8_t flowvol= readDataFlowmeter(); // flowmeter and volume
+}
+
+uint8_t Sensors::readDataDHT() // temperature and humidity function
 {
 	// BUFFER TO RECEIVE
 	uint8_t bits[5];
@@ -72,33 +48,33 @@ uint8_t Sensors::readDataDHT11() // temperature and humidity function
 	for (int i=0; i< 5; i++) bits[i] = 0;
 
 	// REQUEST SAMPLE
-	pinMode(DHT11PIN, OUTPUT);
-	digitalWrite(DHT11PIN, LOW);
+	pinMode(DHT_PIN, OUTPUT);
+	digitalWrite(DHT_PIN, LOW);
 	delay(18);
-	digitalWrite(DHT11PIN, HIGH);
+	digitalWrite(DHT_PIN, HIGH);
 	delayMicroseconds(40);
-	pinMode(DHT11PIN, INPUT);
+	pinMode(DHT_PIN, INPUT);
 
 	// ACKNOWLEDGE or TIMEOUT
 	unsigned int loopCnt = 10000;
-	while(digitalRead(DHT11PIN) == LOW)
+	while(digitalRead(DHT_PIN) == LOW)
 		if (loopCnt-- == 0) return -2; //-2 : timeout
 
 	loopCnt = 10000;
-	while(digitalRead(DHT11PIN) == HIGH)
+	while(digitalRead(DHT_PIN) == HIGH)
 		if (loopCnt-- == 0) return -2; //-2 : timeout
 
 	// READ OUTPUT - 40 BITS => 5 BYTES or TIMEOUT
 	for (int i=0; i<40; i++)
 	{
 		loopCnt = 10000;
-		while(digitalRead(DHT11PIN) == LOW)
+		while(digitalRead(DHT_PIN) == LOW)
 			if (loopCnt-- == 0) return -2; //-2 : timeout
 
 		unsigned long t = micros();
 
 		loopCnt = 10000;
-		while(digitalRead(DHT11PIN) == HIGH)
+		while(digitalRead(DHT_PIN) == HIGH)
 			if (loopCnt-- == 0) return -2; //-2 : timeout
 
 		if ((micros() - t) > 40) bits[idx] |= (1 << cnt);
@@ -112,8 +88,8 @@ uint8_t Sensors::readDataDHT11() // temperature and humidity function
 
 	// WRITE TO RIGHT VARS
         // as bits[1] and bits[3] are allways zero they are omitted in formulas.
-	humidity11    = bits[0] + bits[1]; 
-	temperature11 = bits[2] + bits[3]; 
+	_humidity    = (bits[0] + bits[1])/10; 
+	_temperature = (bits[2] + bits[3])/10; 
 
 	uint8_t sum = bits[0] + bits[1] + bits[2] + bits[3];  
 
@@ -121,75 +97,64 @@ uint8_t Sensors::readDataDHT11() // temperature and humidity function
 	return 0; //  0 : OK
 }
 
-float Sensors::readDataLDR() // LDR function
+uint8_t Sensors::readDataLDR() // LDR function
 {
-  float _minLDR = 5;
-  float _maxLDR = 955;
-  _valueLDR = analogRead(LDR_PIN); // read pin LDR
-  _lastValueLDR = _valueLDR;
-  _valueLDR = constrain(_valueLDR, _minLDR, _maxLDR); // value normalize
+  _valueLDR = map(analogRead(LDR_PIN), _valLDRmin, _valLDRmax, 0, 255);
+  _valueLDR = constrain(_valueLDR, 0, 255);
+  return 0;
   
-  if(_valueLDR < 5) return 0.0;
+  /*if(_valueLDR < 5) return 0.0;
   if(_valueLDR>=5 && _valueLDR <= 36){
     _valueLDR = map(_valueLDR, 5, 36, 1, 10);
-    return (_valueLDR/10);
+    _valueLDR = _valueLDR/10;
+    return 0;
   }
   if(_valueLDR>36 && _valueLDR <= 215){
     _valueLDR = map(_valueLDR, 36, 215, 1, 10);
-    return _valueLDR; 
+    return 0; 
   }
   if(_valueLDR>215 && _valueLDR <= 477){
     _valueLDR = map(_valueLDR, 215, 477, 10, 40);
-    return _valueLDR;
+    return 0;
   }
   if(_valueLDR>477 && _valueLDR <= 673){
     _valueLDR = map(_valueLDR, 477, 673, 40, 100);
-    return _valueLDR;
+    return 0;
   }
   if (_valueLDR>673 && _valueLDR <= 795){
     _valueLDR = map(_valueLDR, 673, 795, 100, 200);
-    return _valueLDR;
+    return 0;
   }
   if(_valueLDR>795 && _valueLDR <= 955){
     _valueLDR = map(_valueLDR, 795, 955, 200, 1000);
-    return _valueLDR;
+    return 0;
   }
   if(_valueLDR>795 && _valueLDR <= 955){
     _valueLDR = map(_valueLDR, 795, 955, 200, 1000);
-    return _valueLDR;
+    return 0;
   }
-  if(_valueLDR>1000) return -2;
+  if(_valueLDR > 1000) return -1;*/
 }
 
-float Sensors::readDataUV() // UV function
-{
-  float _minUV = 0;
-  float _maxUV = 30;
-  _valueUV = analogRead(UV_PIN); // read pinUV
-  _lastValueUV = _valueUV;
-  _valueUV = constrain(_valueUV, _minUV, _maxUV); // value normalize
-  _valueUV = map(_valueUV, _minUV, _maxUV, 0, 15); // Re-map
-  return _valueUV;
+uint8_t Sensors::readDataUV(){ // UV function
+  _valueUV = map(analogRead(UV_PIN), 0, 30, 0, 15); // read pinUV
+  if (_valueUV >= 0.0) return 0;
+  else return -1;
 }
 
-float Sensors::readDataSound(){
- float sound = analogRead(SOUND_PIN); //Sound
- //valorSOUND = constrain(valorSOUND, 0, 100); // value normalize
- sound = map(sound, 0, 1023, 0, 100);
- return sound; 
+uint8_t Sensors::readDataSound(){
+ _sound = map(analogRead(SOUND_PIN), 0, 1023, 0, 100); //Sound
+ if (_sound >= 0.0) return 0;
+ else return -1;
 }
 
-void Sensors::readDataFlowmeter(){
+uint8_t Sensors::readDataFlowmeter(){
  NbTopsFan = 0;   //Set NbTops to 0 ready for calculations
  //sei();      //Enables interrupts
  delay(1000);   //Wait 1 second
  //cli();      //Disable interrupts
- flowmeter = ((NbTopsFan * 60 / 5.5)/60); //(Pulse frequency x 60) / 5.5Q, = flow rate in L/hour 
- volume = ((flowmeter * 60)/1000);
- //Calc = (Calc / 60);
- //Serial.print (Calc, DEC); //Prints the number calculated above
- Serial.print((float)flowmeter, 2);
- Serial.print (" L/min\r\n"); //Prints "L/hour" and returns a  new line
- Serial.print((float)volume, 2);
- Serial.print (" m3/min\r\n"); //Prints "L/hour" and returns a  new line 
+ _flowmeter = ((NbTopsFan * 60 / 5.5)/60); //(Pulse frequency x 60) / 5.5Q, = flow rate in L/hour 
+ _volume = ((_flowmeter * 60)/1000); // m3/min
+ if (_flowmeter >= 0.0 && _volume >= 0.0) return 0;
+ else return -1;
 }
