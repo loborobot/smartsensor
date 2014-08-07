223f1170198c0d5d4c065e267a9a42650d7ef48a
Julio Cesar <julhocesar.7@gmail.com>
2014-08-07 17:49:12 +0200

    Add new methods for connections

---
+++ firmware/Connection.cpp
@@ -0,0 +1,71 @@
+/*#include <GSM.h>
+#include "Connection.h"
+#include "Constants.h"
+
+
+GSMClient client;
+GPRS gprs;
+GSM gsmAccess;
+
+void Connection::begin(){ //init variables
+  Serial.println("Starting Arduino web client");
+  notConnected = true;
+  
+  while(notConnected)
+  {
+    if((gsmAccess.begin(PINNUMBER)==GSM_READY) &
+        (gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD)==GPRS_READY)){
+          IPAddress LocalIP = gprs.getIPAddress();
+          Serial.print("Server IP address="); Serial.println(LocalIP);
+          notConnected = false;
+        }
+    else
+    {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+
+  Serial.println("connecting..");
+}
+
+uint8_t Connection::send(){
+  if(!notConnected){
+    if (client.connect(HTTPGET[0], 8000))
+      {
+        Serial.println("connected");
+        client.println("GET /api/devices/ HTTP/1.0");
+        client.println("Accept: application/json");
+        client.println("Authorization: Basic ZmluY3l0OmZpbmN5dA==");
+        client.println();
+        
+        return 0;
+      } 
+      else
+      {
+        Serial.println("connection failed");
+        return -1;
+      }
+  }
+}
+
+void Connection::read(){
+  if (client.available())
+  {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.available() && !client.connected())
+  {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    for(;;)
+      ;
+  }
+}
+*/
