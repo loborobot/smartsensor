6866d545f3a362a27051d39cd57e367a807fbbef
Julio Cesar <julhocesar.7@gmail.com>
2014-08-08 18:00:32 +0200

    add new methods for sensors

--- firmware/Sensors.h
+++ firmware/Sensors.h
@@ -7,26 +7,35 @@
 class Sensors
 {
 public:
+  float _humidity;
+  float _temperature;
+  int _valueLDR;
+  int _valLDRmin = 1023;
+  int _valLDRmax = 0;
+  
+  float _valueUV;
+  float _sound;
+  float _flowmeter; 
+  float _volume ;
+  float _CO2;
+  float _NO2;
+  
+  //char* _valSensor[9];
+  
   void begin();
   void execute();
   uint8_t updateSensor();
+  char* convertF2C(float val);
   
   uint8_t readDataDHT();
   uint8_t readDataLDR();
   uint8_t readDataUV();
   uint8_t readDataSound();
   uint8_t readDataFlowmeter();
+  uint8_t readDataCO2();
+  uint8_t readDataNO2();
   volatile int NbTopsFan; //measuring the rising edges of the signal
 
 private:
-  float _humidity;
-  float _temperature;
-  int _valueLDR;
-  int _valLDRmin = 1023;
-  int _valLDRmax = 0;
-  float _valueUV;
-  float _sound;
-  float _flowmeter; 
-  float _volume ;
 };
 #endif
